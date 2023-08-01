2ebf7b8772a76ac434c535be01405242c4e03fa1
Dylan Hourigan <dylan.a.hourigan@gmail.com>
2023-08-01 02:47:42 +0100

    Chore: Fixed formatting

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,35 +1,35 @@
 <template>
-    <div class="home">
-      <HeaderNav />
-      <BlogList :posts="blogPosts" />
-    </div>
-  </template>
+  <div class="home">
+    <HeaderNav />
+    <BlogList :posts="blogPosts" />
+  </div>
+</template>
   
-  <script>
-  import BlogList from '@/components/BlogList.vue'; // Import the BlogList component
+<script>
+import BlogList from '@/components/BlogList.vue';
 import HeaderNav from '@/components/HeaderNav.vue';
-  
-  export default {
-    name: 'HomePage',
-    components: {
+
+export default {
+  name: 'HomePage',
+  components: {
     BlogList,
     HeaderNav
-},
-    data() {
-      return {
-        blogPosts: [
-          { id: 1, title: 'First Post', content: 'This is the first blog post.' },
-          { id: 2, title: 'Second Post', content: 'This is the second blog post.' },
-          // Add more blog posts here
-        ],
-      };
-    },
-  };
-  </script>
+  },
+  data() {
+    return {
+      blogPosts: [
+        { id: 1, title: 'First Post', content: 'This is the first blog post.' },
+        { id: 2, title: 'Second Post', content: 'This is the second blog post.' },
+        // Add more blog posts here
+      ],
+    };
+  },
+};
+</script>
   
-  <style>
-  /* Some CSS styling for the Home view */
-  .home {
-    margin: 0 auto;
-  }
-  </style>
+<style>
+/* Some CSS styling for the Home view */
+.home {
+  margin: 0 auto;
+}
+</style>
