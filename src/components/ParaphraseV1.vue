9cf5c705d5dddfd58217bb780c083740ec105532
Dylan Hourigan <80469629+DylanHourigan@users.noreply.github.com>
2023-08-06 22:00:48 +0100

    Feat/re write api integration (#7)
    * Feat(server.js): Added Re-Write-Api integration and /paraphrase endpoint
    
    * Refactor(server.js): Refactored endpoint route in line with changes in API
    
    * Feat(apiService.js): Added API and integrated paraphrase endpoint into view

--- src/components/ParaphraseV1.vue
+++ src/components/ParaphraseV1.vue
@@ -1,23 +1,19 @@
 <template>
   <div class="bg-gray-100 min-h-screen">
     <div class="container mx-auto">
-        <h1 class="text-4xl font-bold pt-4 mb-4">Paraphrasing Tool</h1>
-        <p class="text-lg">Welcome to our paraphrasing tool! Enter a sentence, and our machine learning model will generate a paraphrased version of it. Try it now!</p>
-      </div>
+      <h1 class="text-4xl font-bold pt-4 mb-4">Paraphrasing Tool</h1>
+      <p class="text-lg">Welcome to our paraphrasing tool! Enter a sentence, and our machine learning model will generate
+        a paraphrased version of it. Try it now!</p>
+    </div>
 
     <main class="container mx-auto py-8 px-8">
       <div class="max-w-md mx-auto bg-white rounded-lg shadow-md p-6">
         <h2 class="text-xl font-semibold mb-4">Enter Your Sentence:</h2>
-        <textarea
-          v-model="inputText"
-          class="w-full h-40 p-4 border rounded-md resize-none"
-          placeholder="Enter your sentence here..."
-        ></textarea>
+        <textarea v-model="inputText" class="w-full h-40 p-4 border rounded-md resize-none"
+          placeholder="Enter your sentence here..."></textarea>
 
-        <button
-          @click="paraphrase"
-          class="mt-4 py-2 px-4 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none"
-        >
+        <button @click="paraphrase"
+          class="mt-4 py-2 px-4 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none">
           Paraphrase
         </button>
 
@@ -29,6 +25,8 @@
 </template>
 
 <script>
+import apiService from "@server/apiService";
+
 export default {
   data() {
     return {
@@ -37,15 +35,15 @@ export default {
     };
   },
   methods: {
-    paraphrase() {
-      // Replace this method with your actual machine learning paraphrasing logic
-      // For this example, I'm simply setting the paraphrased text to be the same as the input text
-      this.paraphrasedText = this.inputText;
+    async paraphrase() {
+        try {
+          const data = { input: this.inputText };
+          const response = await apiService.paraphrase(data);
+          this.paraphrasedText = response.data.paraphrase;
+        } catch (error) {
+          console.error(error);
+        }
+      }
     },
-  },
 };
 </script>
-
-<style>
-/* Add any custom styles here */
-</style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path');
 
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/re-write/' : '/',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@server': path.resolve(__dirname, 'server'),
+      },
+    },
+  },
 });
