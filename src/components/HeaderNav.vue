bd8a13d0b3d2296c11829184913d9f1fd53e8d85
Dylan Hourigan <dylan.a.hourigan@gmail.com>
2023-08-13 21:02:51 +0100

    Refactor: Moved svg into assets

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -32,10 +32,8 @@
 
 
         <div v-if="loggedIn" class="md:hidden ml-4">
-          <button class="text-white" @click="toggleMenu">
-            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-            </svg>
+          <button @click="toggleMenu">
+            <img src="@assets/svg/menu.svg" alt="Menu" class="h-6 text-white" /> 
           </button>
         </div>
       </div>
