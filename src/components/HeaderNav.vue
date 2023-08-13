de9d73489a50e146c750e0c6ccc53d5fadaa721d
Dylan Hourigan <dylan.a.hourigan@gmail.com>
2023-08-13 19:43:04 +0100

    Feat: Added the styling for Dark and light mode

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,37 +1,41 @@
 <template>
   <header class="bg-gray-800 py-4">
-    <div class="container mx-auto flex justify-between items-center">
-      <!-- Logo -->
+    <div class="container mx-auto grid grid-cols-3 items-center">
+
       <div class="text-white font-bold text-xl">
         <img src="../assets/logo.jpg" alt="Logo" class="h-20">
       </div>
-      <div class="text-4xl font-bold text-white pl-24 ml-12">Re-Write</div>
-      <!-- Nav Bar -->
-      <nav class="hidden md:flex space-x-4 right-0">
-        <router-link to="/" class="text-white hover:text-gray-200">Home</router-link>
-        <router-link to="/" class="text-white hover:text-gray-200">About</router-link>
-        <router-link to="/" class="text-white hover:text-gray-200">Services</router-link>
-        <router-link to="/login"  @click="handleLogout" class="text-white hover:text-gray-200">Log out</router-link>
-      </nav>
 
-      <!-- Mobile Menu Button (shown on small screens) -->
-      <div class="md:hidden">
-        <button class="text-white">
-          <svg
-            class="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16m-7 6h7"
-            ></path>
-          </svg>
-        </button>
+      <div class="text-4xl font-bold text-white text-center">
+        Re-Write
+      </div>
+
+      <div class="flex justify-end">
+        <nav v-if="loggedIn" class="hidden md:flex space-x-4">
+          <router-link to="/" class="text-white hover:text-gray-200">Home</router-link>
+          <router-link to="/" class="text-white hover:text-gray-200">About</router-link>
+          <router-link to="/" class="text-white hover:text-gray-200">Services</router-link>
+          <router-link to="/login" @click="handleLogout" class="text-white hover:text-gray-200">Log out</router-link>
+        </nav>
+
+        <div class="md:hidden ml-4"> 
+          <button class="text-white">
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   </header>
@@ -47,6 +51,11 @@ export default {
       this.$store.dispatch('user/setLastName', '');
       this.$store.dispatch('user/setAccountType', '');
     },
+  },
+  computed: {
+    loggedIn() {
+      return this.$store.getters['auth/getLoggedIn'] === 'true';
+    }
   },
 };
 </script>
