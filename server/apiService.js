3faeaf2a07deeae139aa95740ebd8c24ada852bd
Dylan Hourigan <dylan.a.hourigan@gmail.com>
2024-02-11 00:12:41 +0000

    Fix: Removed deprecated api link. Added loading symbol

--- server/apiService.js
+++ server/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'https://re-write-api-89b7e8387487.herokuapp.com',
+    baseURL: 'http://localhost:5000',
     headers: {
         'Content-Type': 'application/json',
     },
