126577486f726d13b32e1fc344b3b2eb192372ca
DylanHourigan <dylan.a.hourigan@gmail.com>
2024-04-23 12:31:29 +0100

    Fix: Removed now redundant login functionality

--- src/router/index.js
+++ src/router/index.js
@@ -1,45 +1,22 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-import HomePage from '../views/Homepage.vue';
-import loginPage from '../views/LoginPage.vue';
-import store from '../store';
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomePage from "../views/Homepage.vue";
 
 const routes = [
-    {
-        path: '/',
-        redirect: '/login',
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        component: loginPage,
-    },
-    {
-        path: '/home',
-        name: 'HomePage',
-        component: HomePage
-    },
+  {
+    path: "/",
+    redirect: "/home",
+  },
+  {
+    path: "/home",
+    name: "HomePage",
+    component: HomePage,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    base: process.env.BASE_URL,
-    routes,
-});
-
-router.beforeEach(async (to, from, next) => {
-    const publicPages = ['/login'];
-    const authRequired = !publicPages.includes(to.path);
-    store.dispatch('auth/fetchLoggedIn');
-    const auth = store.getters['auth/getLoggedIn'];
-
-    if (authRequired && auth === 'false') {
-        next('/login');
-    } else if (to.path === '/login' && auth === 'true'){
-        next('/home');
-    } 
-    else {
-        next();
-    }
+  history: createWebHashHistory(),
+  base: process.env.BASE_URL,
+  routes,
 });
 
 
