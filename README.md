bece5efe030d0d3e9f6e73c6271e6b3184a18af6
Dylan Hourigan <dylan.a.hourigan@gmail.com>
2024-05-01 10:52:17 +0100

    changes

--- README.md
+++ README.md
@@ -25,10 +25,7 @@ npm run serve
 The main use of the database at the moment is for authenticating the user, this is subject to change in the future as I further develop the app.
 
 ## Usage
-After launching the application, you'll be prompted to input your credentials.
 
 Once logged in, you'll access a page featuring the paraphrasing tool. Enter a sentence, press the 'paraphrase' button, and the tool will display a rephrased version below the input area. Switch between available models using the dropdown menu.
 
 There are currently two models available: a standard model and a machine learning-driven one.
-
-*Note:* The machine learning model is still under development and might not function optimally.
--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -15,7 +15,6 @@
         <nav v-if="!menuOpen" class="hidden md:flex space-x-4">
           <router-link to="/" class="text-white hover:text-gray-200">Home</router-link>
           <router-link to="/train" class="text-white hover:text-gray-200">Training</router-link>
-          <router-link to="/login" @click="handleLogout" class="text-white hover:text-gray-200">Log out</router-link>
         </nav>
 
         <nav v-if="menuOpen" class="md:hidden space-y-2 bg-slate-700 p-2 absolute w-full top-full right-0 z-10">
@@ -23,8 +22,6 @@
             class="text-white block py-1 px-4 hover:bg-slate-800 border-b border-slate-900">Home</router-link>
           <router-link to="/train"
             class="text-white block py-1 px-4 hover:bg-slate-800 border-b border-slate-900">Train</router-link>
-          <router-link to="/login" @click="handleLogout" class="text-white block py-1 px-4 hover:bg-slate-800">Log
-            out</router-link>
         </nav>
 
         <div v-if="loggedIn" class="md:hidden ml-4">
